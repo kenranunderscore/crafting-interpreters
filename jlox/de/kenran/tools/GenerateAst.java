030a844bffa7ebb11690235053d834db659c7afb
Johannes Maier <johb.maier@gmail.com>
2021-06-02 22:20:11 +0200

    Generate code for the Visitor pattern for expressions

--- jlox/de/kenran/tools/GenerateAst.java
+++ jlox/de/kenran/tools/GenerateAst.java
@@ -33,6 +33,8 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
     // The AST classes.
     for (String type : types) {
       String[] parts = type.split(":");
@@ -40,6 +42,10 @@ public class GenerateAst {
       String fields = parts[1].trim();
       defineType(writer, baseName, className, fields);
     }
+
+    // The base accept() method.
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
     writer.println("}");
     writer.close();
@@ -57,9 +63,35 @@ public class GenerateAst {
     }
 
     writer.println("    }");
+
+    // Visitor pattern.
     writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
+    writer.println("    }");
+
     for (String field : fields) {
       writer.println("    final " + field + ";");
+    }
+
+    writer.println("  }");
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println(
+          "    R visit"
+              + typeName
+              + baseName
+              + "("
+              + typeName
+              + " "
+              + baseName.toLowerCase()
+              + ");");
     }
 
     writer.println("  }");
